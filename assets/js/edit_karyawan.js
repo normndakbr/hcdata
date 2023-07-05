7fb8cd120b5ba9349d5bd18aa79ae24ef2c37110
Normandia Akbar <normnd.akbr2@gmail.com>
2023-07-05 20:34:36 +0800

    Update edit_karyawan.js

--- assets/js/edit_karyawan.js
+++ assets/js/edit_karyawan.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     let idMasterPerusahaan = $("#valuePerusahaan").val();
     let idDepart = $("#valueDepart").val();
+    let auth_depart = "";
     let idPosisi = $("#valuePosisi").val();
+    let auth_posisi = "";
     let idKlasifikasi = $("#valueKlasifikasi").val();
 
     $("#editPerKary").val(idMasterPerusahaan).trigger('change');
@@ -40,27 +42,80 @@ $(document).ready(function () {
                 }
             }
         });
+
+        $.ajax({
+            type: "POST",
+            url: site_url + "departemen/get_auth_depart_by_id",
+            data: {
+                id_depart: idDepart
+            },
+            success: function (res) {
+                let data = JSON.parse(res);
+                auth_depart = data.auth_depart;
+                $("#editDepartKary").val(data.auth_depart).trigger('change');
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log("Error!! " + idDepart);
+                console.log(thrownError);
+            }
+        });
+    }
+
+    function fetch_posisi() {
+        $.ajax({
+            type: "POST",
+            url: site_url + "posisi/get_by_authdepart",
+            data: {
+                auth_depart: auth_depart
+            },
+            success: function (res) {
+                var data = JSON.parse(res);
+                $("#editPosisiKary").removeAttr('disabled');
+                $("#refreshEditPosisi").removeAttr('disabled');
+                $("#editPosisiKary").html(data.posisi);
+                $("#txtEditPosisiKary").LoadingOverlay("hide");
+                $.ajax({
+                    type: "POST",
+                    url: site_url + "posisi/get_auth_posisi_by_id",
+                    data: {
+                        id_posisi: idPosisi
+                    },
+                    success: function (res) {
+                        console.log(res);
+                        let data = JSON.parse(res);
+                        auth_posisi = data.auth_posisi;
+                        $("#editPosisiKary").val(data.auth_posisi).trigger('change');
+                    },
+                    error: function (xhr, ajaxOptions, thrownError) {
+                        console.log("Error!! " + idPosisi);
+                        console.log(thrownError);
+                    }
+                });
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                $("#txtEditPosisiKary").LoadingOverlay("hide");
+                $(".errormsg").removeClass('d-none');
+                $(".errormsg").removeClass('alert-info');
+                $(".errormsg").addClass('alert-danger');
+                if (thrownError != "") {
+                    $(".errormsg").html("Terjadi kesalahan saat load data posisi, hubungi administrator");
+                    $("#addSimpanPekerjaan").remove();
+                }
+            }
+        });
     }
 
     fetch_depart();
 
-    $.ajax({
-        type: "POST",
-        url: site_url + "departemen/get_auth_depart_by_id",
-        data: {
-            id_depart: idDepart
-        },
-        success: function (res) {
-            let data = JSON.parse(res);
-            $("#editDepartKary").val(data.auth_depart).trigger('change');
-
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            console.log("Error!! " + idDepart);
-            console.log(thrownError);
+    $("#editDepartKary").change(function () {
+        if (auth_depart != "") {
+            $("#txtEditPosisiKary").LoadingOverlay("show");
+            fetch_posisi();
+        } else {
+            $("#editPosisiKary").html('<option value="">-- WAJIB DIPILIH --</option>');
+            $("#editPosisiKary").attr('disabled', true);
+            $("#refreshPosisi").attr('disabled', true);
         }
     });
-
-
 
 });
