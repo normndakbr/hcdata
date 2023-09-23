3bdc87b56b991b4e8152af83c7add1fd1db357b0
Normandia Akbar <normnd.akbr2@gmail.com>
2023-09-23 17:38:56 +0800

    Update karydetail.php

--- application/views/dashboard/code/karydetail.php
+++ application/views/dashboard/code/karydetail.php
@@ -1,4 +1,5 @@
 <?php
+echo '<script type="text/javascript" src="' . base_url('assets/js/karyawan.js?auth=' . date('YmdHis')) . '"></script>';
 echo '<script type="text/javascript" src="' . base_url('assets/js/karydetail.js?auth=' . date('YmdHis')) . '"></script>';
 echo '<script type="text/javascript" src="' . base_url('assets/js/edit_personal.js?auth=' . date('YmdHis')) . '"></script>';
 echo '<script type="text/javascript" src="' . base_url('assets/js/edit_karyawan.js?auth=' . date('YmdHis')) . '"></script>';
