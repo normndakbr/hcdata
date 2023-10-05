b885c5da5fb2322f0483b9295264ae978e0408ce
Normandia Akbar <normnd.akbr2@gmail.com>
2023-10-05 08:07:49 +0800

    Update vaksin.php
    update table view

--- application/views/dashboard/karyawan/vaksin.php
+++ application/views/dashboard/karyawan/vaksin.php
@@ -3,11 +3,11 @@
           <table id="tbmVaksin" class="table table-striped table-bordered table-hover text-black text-nowrap" style="width:100%;font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
                <thead>
                     <tr>
-                         <td style="width:1%;text-align:center;">NO.</td>
-                         <td style="width:45%;font-style:bold;">JENIS VAKSIN</td>
-                         <td style="width:45%;font-style:bold;">NAMA VAKSIN</td>
-                         <td style="width:45%;font-style:bold;">TANGGAL VAKSIN</td>
-                         <td style="width:9%;text-align:center;">PROSES</td>
+                         <th style="width:1%; text-align:center;">NO.</th>
+                         <th style="width:45%; font-style:bold; text-align:center;">JENIS VAKSIN</th>
+                         <th style="width:45%; font-style:bold; text-align:center;">NAMA VAKSIN</th>
+                         <th style="width:45%; font-style:bold; text-align:center;">TANGGAL VAKSIN</th>
+                         <th style="width:9%; text-align:center;">PROSES</th>
                     </tr>
                </thead>
                <tbody>
@@ -17,12 +17,16 @@
                          $n = 1;
                          foreach ($vaks as $list) {
                               echo "<tr>";
-                              echo "<td style='ext-align:center;width:1%;'>" . $n++ . "</td>";
+                              echo "<td style='ext-align:center;width:1%; text-align:center;'>" . $n++ . "</td>";
                               echo "<td style='ext-align:center;width:40%;'>" . $list->vaksin_jenis . "</td>";
                               echo "<td style='ext-align:center;width:40%;'>" . $list->vaksin_nama . "</td>";
                               echo "<td style='ext-align:center;width:10%;'>" . date("d-M-Y", strtotime($list->tgl_vaksin)) . "</td>";
                               echo "<td style='ext-align:center;width:9%;'>";
-                              echo "<button id='" . $list->auth_vaksin . "' class='btn btn-danger btn-sm hapusvks'>Hapus</button> ";
+                              if (!empty($status == 'edit')) {
+                                   echo "<button id='" . $list->auth_vaksin . "' class='btn btn-danger btn-sm editHapusVaccine'>Hapus</button> ";
+                              } else {
+                                   echo "<button id='" . $list->auth_vaksin . "' class='btn btn-danger btn-sm hapusvks'>Hapus</button> ";
+                              }
                               echo "</td>";
                               echo "<tr>";
                          }
@@ -37,7 +41,5 @@
                     ?>
                </tbody>
           </table>
-
-          <hr>
      </div>
 </div>
