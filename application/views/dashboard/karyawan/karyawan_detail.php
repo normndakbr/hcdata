17b114fc21fc1932fa1334babce2df991654de89
Normandia Akbar <normnd.akbr2@gmail.com>
2023-10-11 08:53:44 +0800

    Update karyawan_detail.php

--- application/views/dashboard/karyawan/karyawan_detail.php
+++ application/views/dashboard/karyawan/karyawan_detail.php
@@ -32,7 +32,7 @@
                <div class="col-xl-12 col-md-12">
                     <div class="card latest-update-card">
                          <div class="card-header">
-                              <h5>Detail Data Karyawan - <?= "No. KTP : " . $data_kary->no_ktp . " | " . $data_kary->nama_lengkap ?></h5>
+                              <h5><?= $data_kary->no_ktp . " - " . $data_kary->nama_lengkap ?></h5>
                               <div class="card-header-right">
                                    <div class="btn-group card-option">
                                         <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -77,16 +77,29 @@
                                         <div class="tab-content" id="v-pills-tabContent">
                                              <div class="tab-pane fade active show" id="v-pills-dtPersonal" role="tabpanel" aria-labelledby="v-pills-dtPersonal-tab">
                                                   <div class="card-body row">
-                                                       <div class="col-lg-6 col-md-6 col-sm-12">
-                                                            <div class="form-group">
-                                                                 <h6>No. KTP</h6>
-                                                                 <input type="text" class="form-control" value="<?= isset($data_kary->no_ktp) ? $data_kary->no_ktp : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
+                                                       <div class="col-lg-4 col-md-4 col-sm-12 text-center">
+                                                            <div class="m-b-25">
+                                                                 <img style="border: 10px solid #3c3c3c;" src="../../berkas/pasphoto/pasphoto.jpg" width="200" height="200" class="img-radius" alt="User-Profile-Image">
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-6 col-md-6 col-sm-12">
-                                                            <div class="form-group">
-                                                                 <h6>Nama Lengkap</h6>
-                                                                 <input type="text" class="form-control" value="<?= isset($data_kary->nama_lengkap) ? $data_kary->nama_lengkap : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
+                                                       <div class="col-lg-7 col-md-7 col-sm-12 d-flex-row justify-content-center align-items-center">
+                                                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                                                 <div class="form-group">
+                                                                      <h6>No. KTP</h6>
+                                                                      <input type="text" class="form-control" value="<?= isset($data_kary->no_ktp) ? $data_kary->no_ktp : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
+                                                                 </div>
+                                                            </div>
+                                                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                                                 <div class="form-group">
+                                                                      <h6>Nama Lengkap</h6>
+                                                                      <input type="text" class="form-control" value="<?= isset($data_kary->nama_lengkap) ? $data_kary->nama_lengkap : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
+                                                                 </div>
+                                                            </div>
+                                                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                                                 <div class="form-group">
+                                                                      <h6>Tempat & Tanggal Lahir</h6>
+                                                                      <input type="text" class="form-control" value="<?= (isset($data_kary->tmp_lahir) ? $data_kary->tmp_lahir : '-') . ", " . ($data_kary->tgl_lahir == "1970-01-01" ? '' : date('d-M-Y', strtotime($data_kary->tgl_lahir))) ?>" style="background-color:transparent;margin-top:-10px;" disabled>
+                                                                 </div>
                                                             </div>
                                                        </div>
                                                        <div class="col-lg-12 col-md-12 col-sm-12">
@@ -95,55 +108,49 @@
                                                                  <textarea type="text" class="form-control" style="background-color:transparent;margin-top:-10px;" disabled><?= $data_alamat ?></textarea>
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-3 col-md-3 col-sm-12">
+                                                       <div class="col-lg-6 col-md-6 col-sm-12">
                                                             <div class="form-group">
                                                                  <h6>Agama</h6>
                                                                  <input type="text" class="form-control" value="<?= isset($data_kary->agama) ? $data_kary->agama : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-3 col-md-3 col-sm-12">
+                                                       <div class="col-lg-6 col-md-6 col-sm-12">
                                                             <div class="form-group">
                                                                  <h6>Jenis Kelamin</h6>
                                                                  <input type="text" class="form-control" value="<?= isset($data_kary->jk) ? (($data_kary->jk == 'LK') ? "LAKI-LAKI" : "PEREMPUAN") : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-3 col-md-3 col-sm-12">
+                                                       <div class="col-lg-6 col-md-6 col-sm-12">
                                                             <div class="form-group">
                                                                  <h6>Status Pernikahan</h6>
                                                                  <input type="text" class="form-control" value="<?= isset($data_kary->kode_stat_nikah) ? $data_kary->kode_stat_nikah : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-3 col-md-3 col-sm-12">
+                                                       <div class="col-lg-6 col-md-6 col-sm-12">
                                                             <div class="form-group">
                                                                  <h6>Kewarganegaraan</h6>
                                                                  <input type="text" class="form-control" value="<?= isset($data_kary->warga_negara) ? $data_kary->warga_negara : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-4 col-md-6 col-sm-12">
-                                                            <div class="form-group">
-                                                                 <h6>Tempat & Tanggal Lahir</h6>
-                                                                 <input type="text" class="form-control" value="<?= (isset($data_kary->tmp_lahir) ? $data_kary->tmp_lahir : '-') . ", " . ($data_kary->tgl_lahir == "1970-01-01" ? '' : date('d-M-Y', strtotime($data_kary->tgl_lahir))) ?>" style="background-color:transparent;margin-top:-10px;" disabled>
-                                                            </div>
-                                                       </div>
-                                                       <div class="col-lg-4 col-md-6 col-sm-12">
+                                                       <div class="col-lg-6 col-md-6 col-sm-12">
                                                             <div class="form-group">
                                                                  <h6>No. KK</h6>
                                                                  <input type="text" class="form-control" value="<?= isset($data_kary->no_kk) ? $data_kary->no_kk : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-4 col-md-6 col-sm-12">
+                                                       <div class="col-lg-6 col-md-6 col-sm-12">
                                                             <div class="form-group">
                                                                  <h6>No. BPJS Tenaga Kerja</h6>
                                                                  <input type="text" class="form-control" value="<?= isset($data_kary->no_bpjstk) ?  $data_kary->no_bpjstk : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-4 col-md-6 col-sm-12">
+                                                       <div class="col-lg-6 col-md-6 col-sm-12">
                                                             <div class="form-group">
                                                                  <h6>No. BPJS Kesehatan</h6>
                                                                  <input type="text" class="form-control" value="<?= isset($data_kary->no_bpjskes) ?  $data_kary->no_bpjstk : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
                                                             </div>
                                                        </div>
-                                                       <div class="col-lg-4 col-md-6 col-sm-12">
+                                                       <div class="col-lg-6 col-md-6 col-sm-12">
                                                             <div class="form-group">
                                                                  <h6>No. NPWP</h6>
                                                                  <input type="text" class="form-control" value="<?= isset($data_kary->no_npwp) ? $data_kary->no_npwp : '-' ?>" style="background-color:transparent;margin-top:-10px;" disabled>
